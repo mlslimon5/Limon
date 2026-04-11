106d2c94ba1188bc46d02e500b74d82cfe4fdca5
mlslimon5 <limonmls70@gmail.com>
2026-04-11 18:29:59 +0600

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,15 @@
+module.exports = {
+    botName: "Limon Bot 🤖",
+    ownerNumber: "8801623442730@s.whatsapp.net",
+
+    prefix: ".",
+
+    antiBot: false,
+    antiBotKick: true,
+    warnings: {},
+
+    antiLink: false,
+    antiLinkKick: true,
+    antiLinkWarnLimit: 3,
+    linkWarnings: {}
+}
